bbb75ca70ab39f23f4e03ee652d4e0d94fa03401
Mpho Moiloa <mmmphomoiloa@gmail.com>
2026-05-25 16:12:09 +0200

    currently at autoplay animejs

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-import { createTimer, utils } from 'https://esm.sh/animejs';
+import { createTimer, utils, animate } from 'https://esm.sh/animejs';
 
-const [ $loops ] = utils.$('.loops');
-const [ $time ] = utils.$('.time');
+const [ $iterationTime ] = utils.$('.iteration-time');
+const [ $currentTime ] = utils.$('.current-time');
 
-let loops = 0;
+// let loops = 0;
 
 createTimer({
-    loop: true,
-    loopDelay: 750,
-    duration: 250,
+    // loop: true,
+    // loopDelay: 750,
+    duration: 10000,
+    reversed: true,
     //frameRate: 250,
     //delay: 2000,
-    onLoop: () => $loops.innerHTML = ++$loops,
-    onUpdate: self => $time.innerHTML = utils.clamp(self.iterationCurrentTime, 0, 250)
+    // onLoop: () => $loops.innerHTML = ++loops,
+    onUpdate: self => {
+        $iterationTime.innerHTML = self.iterationCurrentTime;
+        $currentTime.innerHTML = self.currentTime
+    }
     // onLoop: self => $count.innerHTML = self.currentIteration
 });
